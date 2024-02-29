fc55556aa1424824f130648c1686b42a463d9e2c
mana-byte <manaiki.laut@centrale-med.fr>
2024-02-29 21:59:33 +0100

    READDED: Startup plugin

--- lua/lazyMana/plugins/esthetics/start_up_config.lua
+++ lua/lazyMana/plugins/esthetics/start_up_config.lua
@@ -1,24 +1,33 @@
 return {
-    -- 'startup-nvim/startup.nvim',
-    -- dependecies = {
-    --     "nvim-telescope/telescope.nvim",
-    --     "nvim-lua/plenary.nvim"
+    {
+        'startup-nvim/startup.nvim',
+        dependecies = {
+            "nvim-telescope/telescope.nvim",
+            "nvim-lua/plenary.nvim"
+        },
+        config = function()
+            local startup = require('startup')
+            startup.setup({
+                theme = 'evil',
+                options = {
+                    mapping_keys = true,
+                    empty_line_between_mappings = true,
+                },
+                mappings = {
+                    execute_command = "<CR>",
+                    open_file = "o",
+                    open_file_split = "<c-o>",
+                    open_section = "<TAB>",
+                    open_help = "?",
+              },
+            })
+        end
+    },
+
+    -- {
+    --     'goolord/alpha-nvim',
+    --     config = function ()
+    --         require'alpha'.setup(require'alpha.themes.dashboard'.config)
+    --     end
     -- },
-    -- config = function()
-    --     local startup = require('startup')
-    --     startup.setup({
-    --         theme = 'evil',
-    --         options = {
-    --             mapping_keys = true,
-    --             empty_line_between_mappings = true,
-    --         },
-    --         mappings = {
-    --             execute_command = "<CR>",
-    --             open_file = "o",
-    --             open_file_split = "<c-o>",
-    --             open_section = "<TAB>",
-    --             open_help = "?",
-    --       },
-    --     })
-    -- end
 }
