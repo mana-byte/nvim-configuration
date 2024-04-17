060c364532821fefe7d13431557974b466d7e4dd
mana-byte <manaiki.laut@centrale-med.fr>
2024-04-17 11:20:13 +0200

    REMOVED COMMENT

--- lua/lazyMana/plugins/autocompletions/lsp_config.lua
+++ lua/lazyMana/plugins/autocompletions/lsp_config.lua
@@ -2,7 +2,7 @@ return {
     {
         'williamboman/mason.nvim',
         config = function()
-            require('mason').setup()         -- using defaults
+            require('mason').setup()
         end
     },
     {
